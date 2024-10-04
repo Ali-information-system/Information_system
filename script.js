02673da4cdc47cab04d1a2f9008db538d5dd4a95
hamza6831 <120903688+hamza6831@users.noreply.github.com>
2024-10-04 09:51:06 +0100

    ok

---
+++ script.js
@@ -0,0 +1,12 @@
+function sumOfMultiples(a,b,l)
+{
+    const l = [0,1,2,3,4,5,10];
+    const a = [2];
+    const b = [5];
+    sum=0;
+    for(i = 0; i<l.length;i++)
+    {
+        if(l[i] % a ==0)
+    }
+    
+}
